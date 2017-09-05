0097d493773fdf1b1fb5f16cb0d6246c7b737dd4
nico24687 <theonlynico@hotmail.com>
2017-09-05 16:25:59 +0000

    refactoring the clean zipcode method to make use of .to_s .r_just and [0..4], which comes under the slicing method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,14 +1,6 @@
 require "csv"
 def clean_zipcode(zipcode)
-    if zipcode.nil?
-        zipcode = "00000"
-    elsif zipcode.length < 5
-        zipcode = zipcode.rjust 5, "0"
-    elsif zipcode.length > 5
-        zipcode = zipcode[0..4]
-    else
-        zipcode
-    end
+    zipcode.to_s.rjust(5,"0")[0..4]
 end
 
 
