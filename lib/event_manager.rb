4661fb27ac19ee064939c480ef96f4e14e26bfbe
nico24687 <theonlynico@hotmail.com>
2017-09-06 12:55:33 +0000

    creating functionality to display legislator names for a given zip code using the sunlight congress API

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,8 @@
 require "csv"
+require "sunlight/congress"
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+
 def clean_zipcode(zipcode)
     zipcode.to_s.rjust(5,"0")[0..4]
 end
@@ -11,6 +15,15 @@ contents = CSV.open "event_attendees.csv", headers: true, header_converters: :sy
 contents.each do |row|
     name = row[:first_name]
     zipcode = clean_zipcode(row[:zipcode])
-    puts "#{name} #{zipcode}"
+    legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+    # legislator_names = []
+    # legislators.each do |legislator|
+    #     legislator_name = "#{legislator.first_name} #{legislator.last_name}"
+    #     legislator_names.push(legislator_name)
+    # end
+    legislator_names = legislators.collect do |legislator|
+        "#{legislator.first_name} #{legislator.last_name}"
+    end
+    puts "#{name} #{zipcode} #{legislator_names}" 
 end 
 
