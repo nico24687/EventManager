44e2f8ab2f47f9d651dde1fb91656460eed77585
nico24687 <theonlynico@hotmail.com>
2017-09-06 13:07:43 +0000

    creating a legislator names method from the previos logic within the contents section

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -7,6 +7,15 @@ def clean_zipcode(zipcode)
     zipcode.to_s.rjust(5,"0")[0..4]
 end
 
+def legislator_names(zipcode)
+      legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+      
+      legislator_names = legislators.collect do |legislator|
+        "#{legislator.first_name} #{legislator.last_name}"
+    end
+    
+      legislator_names.join(", ")
+end
 
 
 puts "EventManager initialized"
@@ -15,15 +24,7 @@ contents = CSV.open "event_attendees.csv", headers: true, header_converters: :sy
 contents.each do |row|
     name = row[:first_name]
     zipcode = clean_zipcode(row[:zipcode])
-    legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
-    # legislator_names = []
-    # legislators.each do |legislator|
-    #     legislator_name = "#{legislator.first_name} #{legislator.last_name}"
-    #     legislator_names.push(legislator_name)
-    # end
-    legislator_names = legislators.collect do |legislator|
-        "#{legislator.first_name} #{legislator.last_name}"
-    end
-    puts "#{name} #{zipcode} #{legislator_names}" 
+    legislators = legislator_names(zipcode)
+    puts "#{name} #{zipcode} #{legislators}" 
 end 
 
